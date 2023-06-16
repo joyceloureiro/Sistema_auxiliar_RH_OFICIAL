f802c336e16e2f908c62f2779a1ad43e0ac5269f
joyce <saoeiisamaa@gmail.com>
2023-06-15 21:52:35 -0300

    ourea

--- iRh/iRh.Windows/simuladores/SistemaSimulacaoCompleta.cs
+++ iRh/iRh.Windows/simuladores/SistemaSimulacaoCompleta.cs
@@ -10,120 +10,7 @@ namespace iRh.Windows.simuladores
             InitializeComponent();
         }
 
-        private void rdPericulosidadeSim_CheckedChanged(object sender, System.EventArgs e)
-        {
-            var salario = double.Parse(txtSalarioBaseSC.Text);
-            var horasNoturnasTrabalhadas = double.Parse(txtHorasNoturnasMesSC.Text);
-            var horasExtrasTrabalhadasMes = double.Parse(txtHorasExtrasTrabalhadasMesSc.Text);
-            var cargaHorariaMes = double.Parse(txtCargaHoraria.Text);
-
-            var valorAdicionalNoturno = AdicionalNoturno.calculaAD(salario, horasNoturnasTrabalhadas, cargaHorariaMes);
-            var valorHoraExtra = HoraExtra.calculaHoraExtra(salario, horasExtrasTrabalhadasMes, cargaHorariaMes);
-            var valorAdicionalPericulosidade = Periculosidade.calculaPericulosidade(salario);
-            var valorValeTransporte = ValeTransporte.calculaVT(salario);
-            var descontoInss = Inss.Calcula(salario);
-            var valorIrrf = Irrf.CalculaIrrf(salario);
-            var valorFgts = Fgts.calculaFgts(salario);
-          
-
-            if ( rdValeTransporteSim_CheckedChanged)
-            {
-                if (string.IsNullOrEmpty(txtSalarioBaseSC.Text))
-                {
-                    MessageBox.Show("Informe seu sálario base: ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    txtSalarioBaseSC.Focus();
-                    return;
-                }
-                try
-                {
-                    var salario = double.Parse(txtSalarioBaseSC.Text);
-                    var horasNoturnasTrabalhadas = double.Parse(txtHorasNoturnasMesSC.Text);
-                    var horasExtrasTrabalhadasMes = double.Parse(txtHorasExtrasTrabalhadasMesSc.Text);
-                    var cargaHorariaMes = double.Parse(txtCargaHoraria.Text);
-
-                    var valorAdicionalNoturno = AdicionalNoturno.calculaAD(salario, horasNoturnasTrabalhadas, cargaHorariaMes);
-                    var valorHoraExtra = HoraExtra.calculaHoraExtra(salario, horasExtrasTrabalhadasMes, cargaHorariaMes);
-                    var valorAdicionalPericulosidade = Periculosidade.calculaPericulosidade(salario);
-                    var valorValeTransporte = ValeTransporte.calculaVT(salario);
-                    var descontoInss = Inss.Calcula(salario);
-                    var valorIrrf = Irrf.CalculaIrrf(salario);
-                    var valorFgts = Fgts.calculaFgts(salario);
-
-                    var salarioComAdicionais = salario + valorAdicionalNoturno + valorHoraExtra + valorAdicionalPericulosidade + valorValeTransporte;
-                    var salarioComDescontos = salarioComAdicionais - valorIrrf - descontoInss - valorValeTransporte - valorFgts;
-
-                    lblMostrarResultadoSimulacaoCompleta.Text = "valor salario liquído: " + salarioComDescontos.ToString("C");
-                    lblMostrarResultadoSimulacaoCompleta.Visible = true;
-
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("Informe seu sálario base: ex:1506", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    txtSalarioBaseSC.Focus();
-
-                    MessageBox.Show("Informe sua horas trabalhadas no mes: ex:1506", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    txtCargaHoraria.Focus();
-
-                    MessageBox.Show("Informe sua horas extras trabalhadas no mes: ex:1506", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    txtHorasExtrasTrabalhadasMesSc.Focus();
-
-                    MessageBox.Show("Informe sua hora noturna trabalhada no mes: ex:1506", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    txtHorasNoturnasMesSC.Focus();
-                    throw;
-                }
-                panelResultadoSimulacaoCompleta.Show();
-            }
-            else if(rdValeTransporteNao_CheckedChanged)
-            {
-               
-                {
-                    if (string.IsNullOrEmpty(txtSalarioBaseSC.Text))
-                    {
-                        MessageBox.Show("Informe seu sálario base: ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        txtSalarioBaseSC.Focus();
-                        return;
-                    }
-                    try
-                    {
-                        var salario = double.Parse(txtSalarioBaseSC.Text);
-                        var horasNoturnasTrabalhadas = double.Parse(txtHorasNoturnasMesSC.Text);
-                        var horasExtrasTrabalhadasMes = double.Parse(txtHorasExtrasTrabalhadasMesSc.Text);
-                        var cargaHorariaMes = double.Parse(txtCargaHoraria.Text);
-
-                        var valorAdicionalNoturno = AdicionalNoturno.calculaAD(salario, horasNoturnasTrabalhadas, cargaHorariaMes);
-                        var valorHoraExtra = HoraExtra.calculaHoraExtra(salario, horasExtrasTrabalhadasMes, cargaHorariaMes);
-                        var valorAdicionalPericulosidade = Periculosidade.calculaPericulosidade(salario);
-                       
-                        var descontoInss = Inss.Calcula(salario);
-                        var valorIrrf = Irrf.CalculaIrrf(salario);
-                        var valorFgts = Fgts.calculaFgts(salario);
-
-                        var salarioComAdicionais = salario + valorAdicionalNoturno + valorHoraExtra + valorAdicionalPericulosidade + valorValeTransporte;
-                        var salarioComDescontos = salarioComAdicionais - valorIrrf - descontoInss  - valorFgts;
-
-                        lblMostrarResultadoSimulacaoCompleta.Text = "valor salario liquído: " + salarioComDescontos.ToString("C");
-                        lblMostrarResultadoSimulacaoCompleta.Visible = true;
-
-                    }
-                    catch (Exception)
-                    {
-                        MessageBox.Show("Informe seu sálario base: ex:1506", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        txtSalarioBaseSC.Focus();
-
-                        MessageBox.Show("Informe sua horas trabalhadas no mes: ex:1506", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        txtCargaHoraria.Focus();
-
-                        MessageBox.Show("Informe sua horas extras trabalhadas no mes: ex:1506", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        txtHorasExtrasTrabalhadasMesSc.Focus();
-
-                        MessageBox.Show("Informe sua hora noturna trabalhada no mes: ex:1506", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        txtHorasNoturnasMesSC.Focus();
-                        throw;
-                    }
-                    panelResultadoSimulacaoCompleta.Show();
-                }
-            }
-        }
+       
 
         private void rdPericulosidadeNao_CheckedChanged(object sender, System.EventArgs e)
         {
@@ -132,63 +19,9 @@ namespace iRh.Windows.simuladores
 
         private void btnCalcularSC_Click(object sender, System.EventArgs e)
         {
-
-        }
-    }
-
-
-    private void rdPericulosidadeSim_CheckedChanged(object sender, System.EventArgs e)
-        {
-            if ( rdValeTransporteSim.Checked)
+            if (rdPericulosidadeNao.Checked)
             {
-                if (string.IsNullOrEmpty(txtSalarioBaseSC.Text))
-                {
-                    MessageBox.Show("Informe seu sálario base: ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    txtSalarioBaseSC.Focus();
-                    return;
-                }
-                try
-                {
-                    var salario = double.Parse(txtSalarioBaseSC.Text);
-                    var horasNoturnasTrabalhadas = double.Parse(txtHorasNoturnasMesSC.Text);
-                    var horasExtrasTrabalhadasMes = double.Parse(txtHorasExtrasTrabalhadasMesSc.Text);
-                    var cargaHorariaMes = double.Parse(txtCargaHoraria.Text);
-
-                    var valorAdicionalNoturno = AdicionalNoturno.calculaAD(salario, horasNoturnasTrabalhadas, cargaHorariaMes);
-                    var valorHoraExtra = HoraExtra.calculaHoraExtra(salario, horasExtrasTrabalhadasMes, cargaHorariaMes);
-                    var valorAdicionalPericulosidade = Periculosidade.calculaPericulosidade(salario);
-                    var valorValeTransporte = ValeTransporte.calculaVT(salario);
-                    var descontoInss = Inss.Calcula(salario);
-                    var valorIrrf = Irrf.CalculaIrrf(salario);
-                    var valorFgts = Fgts.calculaFgts(salario);
-
-                    var salarioComAdicionais = salario + valorAdicionalNoturno + valorHoraExtra + valorAdicionalPericulosidade + valorValeTransporte;
-                    var salarioComDescontos = salarioComAdicionais - valorIrrf - descontoInss - valorValeTransporte - valorFgts;
-
-                    lblMostrarResultadoSimulacaoCompleta.Text = "valor salario liquído: " + salarioComDescontos.ToString("C");
-                    lblMostrarResultadoSimulacaoCompleta.Visible = true;
-
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("Informe seu sálario base: ex:1506", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    txtSalarioBaseSC.Focus();
-
-                    MessageBox.Show("Informe sua horas trabalhadas no mes: ex:1506", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    txtCargaHoraria.Focus();
-
-                    MessageBox.Show("Informe sua horas extras trabalhadas no mes: ex:1506", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    txtHorasExtrasTrabalhadasMesSc.Focus();
-
-                    MessageBox.Show("Informe sua hora noturna trabalhada no mes: ex:1506", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    txtHorasNoturnasMesSC.Focus();
-                    throw;
-                }
-                panelResultadoSimulacaoCompleta.Show();
-            }
-            else if(rdValeTransporteNao_CheckedChanged)
-            {
-               
+                if (rdPericulosidadeNao.Checked)
                 {
                     if (string.IsNullOrEmpty(txtSalarioBaseSC.Text))
                     {
@@ -206,13 +39,13 @@ namespace iRh.Windows.simuladores
                         var valorAdicionalNoturno = AdicionalNoturno.calculaAD(salario, horasNoturnasTrabalhadas, cargaHorariaMes);
                         var valorHoraExtra = HoraExtra.calculaHoraExtra(salario, horasExtrasTrabalhadasMes, cargaHorariaMes);
                         var valorAdicionalPericulosidade = Periculosidade.calculaPericulosidade(salario);
-                       
+                        var valorValeTransporte = ValeTransporte.calculaVT(salario);
                         var descontoInss = Inss.Calcula(salario);
                         var valorIrrf = Irrf.CalculaIrrf(salario);
                         var valorFgts = Fgts.calculaFgts(salario);
 
                         var salarioComAdicionais = salario + valorAdicionalNoturno + valorHoraExtra + valorAdicionalPericulosidade + valorValeTransporte;
-                        var salarioComDescontos = salarioComAdicionais - valorIrrf - descontoInss  - valorFgts;
+                        var salarioComDescontos = salarioComAdicionais - valorIrrf - descontoInss - valorValeTransporte - valorFgts;
 
                         lblMostrarResultadoSimulacaoCompleta.Text = "valor salario liquído: " + salarioComDescontos.ToString("C");
                         lblMostrarResultadoSimulacaoCompleta.Visible = true;
@@ -233,10 +66,109 @@ namespace iRh.Windows.simuladores
                         txtHorasNoturnasMesSC.Focus();
                         throw;
                     }
-                    panelResultadoSimulacaoCompleta.Show();
                 }
+                panelResultadoSimulacaoCompleta.Show();
             }
-                 
-                    
+            else
+            {
+                if (rdPericulosidadeNao.Checked)
+                {
+                    if (string.IsNullOrEmpty(txtSalarioBaseSC.Text))
+                    {
+                        MessageBox.Show("Informe seu sálario base: ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txtSalarioBaseSC.Focus();
+                        return;
+                    }
+                    try
+                    {
+                        var salario = double.Parse(txtSalarioBaseSC.Text);
+                        var horasNoturnasTrabalhadas = double.Parse(txtHorasNoturnasMesSC.Text);
+                        var horasExtrasTrabalhadasMes = double.Parse(txtHorasExtrasTrabalhadasMesSc.Text);
+                        var cargaHorariaMes = double.Parse(txtCargaHoraria.Text);
+
+                        var valorAdicionalNoturno = AdicionalNoturno.calculaAD(salario, horasNoturnasTrabalhadas, cargaHorariaMes);
+                        var valorHoraExtra = HoraExtra.calculaHoraExtra(salario, horasExtrasTrabalhadasMes, cargaHorariaMes);
+                        var valorAdicionalPericulosidade = Periculosidade.calculaPericulosidade(salario);
+                        var valorValeTransporte = ValeTransporte.calculaVT(salario);
+                        var descontoInss = Inss.Calcula(salario);
+                        var valorIrrf = Irrf.CalculaIrrf(salario);
+                        var valorFgts = Fgts.calculaFgts(salario);
+
+                        var salarioComAdicionais = salario + valorAdicionalNoturno + valorHoraExtra + valorAdicionalPericulosidade + valorValeTransporte;
+                        var salarioComDescontos = salarioComAdicionais - valorIrrf - descontoInss - valorValeTransporte - valorFgts;
+
+                        lblMostrarResultadoSimulacaoCompleta.Text = "valor salario liquído: " + salarioComDescontos.ToString("C");
+                        lblMostrarResultadoSimulacaoCompleta.Visible = true;
+
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Informe seu sálario base: ex:1506", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txtSalarioBaseSC.Focus();
+
+                        MessageBox.Show("Informe sua horas trabalhadas no mes: ex:1506", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txtCargaHoraria.Focus();
+
+                        MessageBox.Show("Informe sua horas extras trabalhadas no mes: ex:1506", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txtHorasExtrasTrabalhadasMesSc.Focus();
+
+                        MessageBox.Show("Informe sua hora noturna trabalhada no mes: ex:1506", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txtHorasNoturnasMesSC.Focus();
+                        throw;
+                    }
+                }
+                panelResultadoSimulacaoCompleta.Show();
+                else
+                {
+                    if (string.IsNullOrEmpty(txtSalarioBaseSC.Text))
+                    {
+                        MessageBox.Show("Informe seu sálario base: ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txtSalarioBaseSC.Focus();
+                        return;
+                    }
+                    try
+                    {
+                        var salario = double.Parse(txtSalarioBaseSC.Text);
+                        var horasNoturnasTrabalhadas = double.Parse(txtHorasNoturnasMesSC.Text);
+                        var horasExtrasTrabalhadasMes = double.Parse(txtHorasExtrasTrabalhadasMesSc.Text);
+                        var cargaHorariaMes = double.Parse(txtCargaHoraria.Text);
+
+                        var valorAdicionalNoturno = AdicionalNoturno.calculaAD(salario, horasNoturnasTrabalhadas, cargaHorariaMes);
+                        var valorHoraExtra = HoraExtra.calculaHoraExtra(salario, horasExtrasTrabalhadasMes, cargaHorariaMes);
+                        var valorAdicionalPericulosidade = Periculosidade.calculaPericulosidade(salario);
+                        var valorValeTransporte = ValeTransporte.calculaVT(salario);
+                        var descontoInss = Inss.Calcula(salario);
+                        var valorIrrf = Irrf.CalculaIrrf(salario);
+                        var valorFgts = Fgts.calculaFgts(salario);
+
+                        var salarioComAdicionais = salario + valorAdicionalNoturno + valorHoraExtra + valorAdicionalPericulosidade + valorValeTransporte;
+                        var salarioComDescontos = salarioComAdicionais - valorIrrf - descontoInss - valorValeTransporte - valorFgts;
+
+                        lblMostrarResultadoSimulacaoCompleta.Text = "valor salario liquído: " + salarioComDescontos.ToString("C");
+                        lblMostrarResultadoSimulacaoCompleta.Visible = true;
+
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Informe seu sálario base: ex:1506", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txtSalarioBaseSC.Focus();
+
+                        MessageBox.Show("Informe sua horas trabalhadas no mes: ex:1506", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txtCargaHoraria.Focus();
+
+                        MessageBox.Show("Informe sua horas extras trabalhadas no mes: ex:1506", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txtHorasExtrasTrabalhadasMesSc.Focus();
+
+                        MessageBox.Show("Informe sua hora noturna trabalhada no mes: ex:1506", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txtHorasNoturnasMesSC.Focus();
+                        throw;
+                    }
+                }
+                panelResultadoSimulacaoCompleta.Show();
+            }
+        }
+    }
+
+
     }
 }
